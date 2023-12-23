7c0c50846b3b4ad9f4a68a52d81b44238fa6231e
vbalmeida <vb.almeida22@gmail.com>
2023-12-23 18:42:35 -0300

    Desafio Gerenciamento de tarefas

---
+++ Models/Program.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace desafio_csharp_gerenciamento_de_tarefas.Models
+{
+    public class Program
+    {
+        
+    }
+}
---
+++ Programa.cs
@@ -0,0 +1,19 @@
+﻿string titulo;
+string descricao;
+string dataVencimento;
+
+titulo = Console.ReadLine();
+descricao = Console.ReadLine();
+
+if (descricao.Length < 50)
+{
+    dataVencimento = Console.ReadLine();
+    Console.WriteLine(descricao + " ate " + dataVencimento );
+}
+else
+{ 
+    Console.WriteLine("Descricao ultrapassa limite de caracteres.");
+}               
+          
+        
+    
